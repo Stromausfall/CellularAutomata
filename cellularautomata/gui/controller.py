2458d735bf75f940ccbe135017be75446ccd4e1f
Stromausfall <mail@matthias-auer.net>
2019-01-12 21:53:44 +0100

    implemented calculation thread - app works

--- cellularautomata/gui/controller.py
+++ cellularautomata/gui/controller.py
@@ -1,3 +1,5 @@
+import numpy
+from kivy.clock import Clock
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -6,31 +8,62 @@ from kivy.uix.screenmanager import ScreenManager
 
 from cellularautomata.gui.setupmap.clickabletile import ClickableTile
 from cellularautomata.gui.setupmap.unclickabletile import UnclickableTile
+from cellularautomata.logic.calculationinteraction import CalculationInteraction
 
 
 class Controller(BoxLayout):
-    setup_screen_map_size = ObjectProperty()
+    simulation_screen_content = ObjectProperty()
+
+    def __init__(self):
+        BoxLayout.__init__(self)
+        self.__calculation_thread_interaction:CalculationInteraction = None
 
     def finish_setup_settings_screen(self, screen_manager: ScreenManager, container: GridLayout):
         screen_manager.current = "setup_map_screen"
 
-        # file the setup map screen with buttons
+        # fill the setup map screen with buttons
         for row in range(container.rows):
             for column in range(container.cols):
                 button: Button = ClickableTile(row=row, column=column)
 
                 container.add_widget(button)
 
-    def finish_setup_map_screen(self, setup_map_screen_content: GridLayout, simulation_screen_content: GridLayout,
-                                screen_manager: ScreenManager):
+    def get_ndarray_from(self, grid_layout: GridLayout) -> numpy.ndarray:
+        biggest_tile: ClickableTile = sorted(grid_layout.children)[-1]
+
+        result: numpy.ndarray = numpy.empty(shape=(biggest_tile.row+1, biggest_tile.column+1))
+        for tile in grid_layout.children:
+            result[tile.row][tile.column] = tile.is_selected
+
+        return result
+
+    def finish_setup_map_screen(self, screen_manager: ScreenManager, setup_map_screen_content: GridLayout):
+        # create unclickable tiles and add them to the screen
         for tile in sorted(setup_map_screen_content.children):
             clickableTile: ClickableTile = tile
             unclickableTile: UnclickableTile = UnclickableTile(row=clickableTile.row, column=clickableTile.column,
                                                                is_alive=clickableTile.is_selected)
 
-            simulation_screen_content.add_widget(unclickableTile)
+            self.simulation_screen_content.add_widget(unclickableTile)
 
+        # switch screen
         screen_manager.current = "simulation_screen"
+
+        # create the CalculationThreadInteraction instance
+        self.__calculation_thread_interaction = CalculationInteraction(
+            self.get_ndarray_from(setup_map_screen_content))
+
+        # start calling the callback every frame
+        Clock.schedule_interval(self.display_newest_world, 0)
+
+    def display_newest_world(self, *largs):
+        newest_world:numpy.ndarray = self.__calculation_thread_interaction.get_newest_world()
+
+        for tile in self.simulation_screen_content.children:
+            tile.is_alive = newest_world[tile.row][tile.column]
+
+    def simulation_speed_slider_value_change(self, value:float):
+        self.__calculation_thread_interaction.change_generations_per_second(value)
 
     def setup_settings_screen_map_size_text(self, map_size: int):
         return str(map_size) + "x" + str(map_size)
--- cellularautomata/logic/calculationthread.py
+++ cellularautomata/logic/calculationthread.py
@@ -4,49 +4,112 @@ import threading
 import numpy
 
 
-# e = threading.Event()
-
-
 class CalculationThread(threading.Thread):
     def __init__(self,
                  initial_data: numpy.ndarray,
                  generations_per_second_change_event: threading.Event,
                  generations_per_second_queue: queue.Queue,
                  finished_world_queue: queue.Queue):
-        threading.Thread.__init__(self)
+        # this should be a daemon tread (it alone doesn't keep the program alive)
+        threading.Thread.__init__(self, daemon=True)
 
-        pass
+        self.__current_world: numpy.ndarray = initial_data
+        self.__generations_per_second_change_event: threading.Event = generations_per_second_change_event
+        self.__generations_per_second_queue: queue.Queue = generations_per_second_queue
+        self.__finished_world_queue: queue.Queue = finished_world_queue
+        self.__wait_time: int = 0
+
+    def __pause_until_next_generation(self):
+        # get the latest wait time
+        while not self.__generations_per_second_queue.empty():
+            self.__wait_time = self.__generations_per_second_queue.get_nowait()
+
+        disrupted_wait: bool = False
+
+        if self.__wait_time == 0:
+            disrupted_wait = self.__generations_per_second_change_event.wait()
+        else:
+            disrupted_wait = self.__generations_per_second_change_event.wait(1 / self.__wait_time)
+
+        # finished waiting - clear the event
+        self.__generations_per_second_change_event.clear()
+
+        # if the wait was disrupted - wait again !
+        if disrupted_wait:
+            self.__pause_until_next_generation()
 
     def run(self):
-        pass
-#         print("hi, this is thread #%s and i'm sleeping :)" % self.xxx)
-#         event_was_set: bool = e.wait(timeout=self.xxx)
-#         print("event was set = %s" % event_was_set)
-#
-#
-# class NotifyThread(threading.Thread):
-#     def __init__(self, xxx):
-#         threading.Thread.__init__(self)
-#         self.xxx = xxx
-#
-#     def run(self):
-#         time.sleep(self.xxx)
-#         print("hi, this is thread #%s and after sleeping i wake the other thread up!" % self.xxx)
-#         e.set()
-#
-#
-# list = [4, 3, 2, 1, 2, 3, 4]
-#
-#
-# def foo(row, column) -> bool:
-#     return list[row] == column
-#
-#
-# func_foo: function = foo
-#
-# CalculationThread(20).start()
-# NotifyThread(25).start()
-#
-# x = numpy.zeros((3, 4))
-#
-# print(x)
+        while True:
+            # wait
+            self.__pause_until_next_generation()
+
+            # calculate the new world
+            self.__current_world = self.calculate_new_world(self.__current_world)
+
+            # push the new world into the queue
+            self.__finished_world_queue.put_nowait(self.__current_world)
+
+    def get_cell_wrapped_around(self, world: numpy.ndarray, x: int, y: int) -> bool:
+        # handle wrap around for x
+        if x == world.shape[0]:
+            x = 0
+        if x == -1:
+            x = world.shape[0] - 1
+
+        # handle wrap around for y
+        if y == world.shape[1]:
+            y = 0
+        if y == -1:
+            y = world.shape[1] - 1
+
+        return world[x][y]
+
+    def get_neighbor_coordinates(self, x: int, y: int) -> list:
+        result = []
+
+        for neighbor_x in range(x - 1, x + 2):
+            for neighbor_y in range(y - 1, y + 2):
+                result += [(neighbor_x, neighbor_y)]
+
+        result.remove((x, y))
+
+        return result
+
+    def calculate_number_of_live_cells(self, world: numpy.ndarray, x: int, y: int) -> int:
+        neighbor_coordinates = self.get_neighbor_coordinates(x, y)
+        neighbor_alive = [self.get_cell_wrapped_around(world, coordinates[0], coordinates[1]) for coordinates in
+                          neighbor_coordinates]
+
+        return neighbor_alive.count(True)
+
+    def is_cell_alive(self, world: numpy.ndarray, x: int, y: int):
+        alive_neighbors: int = self.calculate_number_of_live_cells(world, x, y)
+        is_currently_alive: bool = world[x][y]
+
+        if is_currently_alive:
+            if alive_neighbors < 2:
+                # cell dies of starvation
+                return False
+            elif alive_neighbors in [2, 3]:
+                # with 2 or 3 alive neighbors the cell continues to live
+                return True
+            else:
+                # the cell dies of overpopulation (more than 3 alive neighbors
+                return False
+        else:
+            if alive_neighbors == 3:
+                # cell becomes alive through reproduction
+                return True
+            else:
+                # cell stays dead
+                return False
+
+    def calculate_new_world(self, old_world: numpy.ndarray) -> numpy.ndarray:
+        new_world: numpy.ndarray = numpy.empty(shape=old_world.shape)
+        new_world.fill(False)
+
+        for x in range(new_world.shape[0]):
+            for y in range(new_world.shape[1]):
+                new_world[x][y] = self.is_cell_alive(old_world, x, y)
+
+        return new_world
